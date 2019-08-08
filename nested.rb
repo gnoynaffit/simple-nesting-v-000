48e634838d962c972eaececead276b5bfc611bc6
Tiffany <ong.tifffany1503@gmail.com>
2019-08-08 01:49:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -39,6 +39,8 @@ def alan_kay_is_known_for
      }
 end
 
+alan_kay_is_known_for
+
 def dennis_ritchies_language
 	programmer_hash = 
  		{
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash [:dennis_ritchie] [:languages] [0]
 end
 
 def adding_matz
@@ -80,7 +83,7 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash
 end
 
 def changing_alan
@@ -102,7 +105,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +125,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 
 end
+
+adding_to_dennis
